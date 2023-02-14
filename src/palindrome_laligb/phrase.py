caa8d9ac2ab0f9c52c1b8226b295654cbfb5e455
Lali Bibilashvili <lalibibilashvili@gmail.com>
2023-02-14 11:12:18 +0100

    my version

--- src/palindrome_laligb/phrase.py
+++ src/palindrome_laligb/phrase.py
@@ -4,11 +4,13 @@ class Phrase:
     """A class to represent phrases."""
 
     def __init__(self, content):
-        self.content = content
+        # self.content = content
+        self.content = str(content)
 
     def processed_content(self):
         """Process the content for palindrome testing."""
-        return self.letters().lower()
+        # return self.letters().lower()
+        return self.letters_and_digits().lower()
 
     def ispalindrome(self):
         """Return True for a palindrome, False otherwise."""
@@ -17,6 +19,10 @@ class Phrase:
     def letters(self):
         """Return the letters in the content."""
         return "".join(re.findall(r"[a-zA-Z]", self.content))
+
+    def letters_and_digits(self):
+        """Return the letters and digits in the content."""
+        return "".join(re.findall(r"[a-zA-Z\d]", self.content))
 
 
 def reverse(string):
--- tests/test_phrase.py
+++ tests/test_phrase.py
@@ -16,3 +16,12 @@ def test_palindrome_with_punctuation():
 
 def test_letters():
     assert Phrase("Madam, I'm Adam.").letters() == "MadamImAdam"
+
+def test_letters_and_digits():
+    assert Phrase("Madam, I'm Adam.").letters_and_digits() == "MadamImAdam"
+
+def test_integer_non_palindrome():
+    assert not Phrase(12345).ispalindrome()
+
+def test_integer_palindrome():
+    assert Phrase(12321).ispalindrome()
